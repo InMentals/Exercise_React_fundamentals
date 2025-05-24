3f77877acbb70fab63ebb7946a7856b2b82e709c
Pablo Cervigon <pablocervigon@gmail.com>
2025-05-24 02:13:48 +0200

    define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import AdvertsPage from "./pages/adverts/adverts-page";
 import LoginPage from "./pages/auth/login-page";
-import { useAuth } from "./pages/auth/context";
 import NewAdvertPage from "./pages/adverts/new-advert-page";
+import { Navigate, Route, Routes } from "react-router";
 
 function App() {
-  const { isLogged } = useAuth();
-  return isLogged ? (
-    <>
-      <AdvertsPage />
-      <NewAdvertPage />
-    </>
-  ) : (
-    <LoginPage />
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/adverts" element={<AdvertsPage />} />
+      <Route path="/adverts/new" element={<NewAdvertPage />} />
+      <Route path="/" element={<Navigate to="/adverts" />} />
+      <Route path="/not-found" element={<div>404 | Not found</div>} />
+      <Route path="*" element={<Navigate to="/not-found" />} />
+    </Routes>
   );
 }
 
